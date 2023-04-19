17419495e76220f1807108782e4f19b4e21d4030
giovanni-cal <61541556+giovanni-cal@users.noreply.github.com>
2023-04-19 17:51:21 +0200

    Delete sum_global_costs.m

--- sum_global_costs.m
+++
@@ -1,8 +0,0 @@
-function F = sum_global_costs(r,phi_B,H_B,v,tau_s,c_V,c_M,mu_W,mu_A,mu_T,Prob_rR,tot_Dem)
-
-    [F_V,F_M,F_W,F_T,F_transf] = global_cost_fun(r,phi_B,H_B,v,tau_s,...
-        c_V,c_M,mu_W,mu_A,mu_T,Prob_rR,tot_Dem);
-
-    F = F_V + F_M + F_W + F_T + F_transf;
-    
-end
